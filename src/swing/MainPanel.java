8cd58e7c74d2d857934dfd949ef504f5de324b3a
M. Alfan Septian Nufi <alfan.s.nufi@gmail.com>
2022-11-27 21:50:02 +0700

    Add files via upload

---
+++ src/swing/MainPanel.java
@@ -0,0 +1,18 @@
+
+package swing;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+
+public class MainPanel extends JPanel{
+
+    public MainPanel() {
+        setOpaque(false);
+    }
+
+
+    
+    
+    
+}
